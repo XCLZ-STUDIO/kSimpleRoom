4562b4be60ede9726aaa3f33942135b1c8ec881d
小涂 <1206294056@qq.com>
2022-04-05 22:50:25 +0800

    refactor：更改项目总名称
    改为korom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
 }
 
 
-rootProject.name = "kSimpleRoom"
+rootProject.name = "korom"
 include("protocol", "server", "client")
 
 project(":protocol").name = "korom-protocol"
